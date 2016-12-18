4e910342a13a7fd1b092dbf440ec2cb6ba700620
Nicolaica Catalin <ncatalin_1988@yahoo.com>
2016-12-18 20:14:49 +0200

    finished faq page and removed unecesarry file like jquery ui also added tingle.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,34 @@
-// Acordion for FAQ Page
-$(".accordion").accordion({
-    active: false,
-    collapsible: true,
-});
+// form 
 
 $.validate();
+
+// instanciate new modal
+var modal = new tingle.modal({
+  footer: true,
+  stickyFooter: false,
+  closeLabel: "Close",
+  onOpen: function() {
+  },
+  onClose: function() {
+  },
+  beforeClose: function() {
+    // here's goes some logic
+    // e.g. save content before closing the modal
+    return true; // close the modal
+    return false; // nothing happens
+  }
+});
+
+var modalBigContent = new tingle.modal();
+// window one
+var tingle_button = document.querySelector('.tingle_addon');
+tingle_button.addEventListener('click', function(){
+  modalBigContent.open();
+});
+modalBigContent.setContent(document.querySelector('.tingle_addon_window').innerHTML);
+// window two
+var tingle_button_two = document.querySelector('.tingle_addon_two');
+tingle_button_two.addEventListener('click', function(){
+  modalBigContent.open();
+modalBigContent.setContent(document.querySelector('.tingle_addon_window_two').innerHTML);
+});
